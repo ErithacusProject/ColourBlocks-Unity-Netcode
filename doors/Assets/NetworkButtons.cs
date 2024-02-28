f3be94d30e9302acc9ae7cd5623b0b58f424ad88
Robyn <92083124+AleneVedMidnat@users.noreply.github.com>
2024-02-28 15:30:46 +0000

    ui collapse

--- doors/Assets/NetworkButtons.cs
+++ doors/Assets/NetworkButtons.cs
@@ -10,36 +10,37 @@ public class NetworkButtons : NetworkBehaviour
     [SerializeField] Button ServerBtn;
     [SerializeField] Button ClientBtn;
     [SerializeField] GameObject playerPrefab;
-    gameInfo gameInfo;
+    int clickCount = 0;
 
     //ulong ClientID = 0;
 
     private void Awake()
     {
-        gameInfo = FindAnyObjectByType<gameInfo>();
-        HostBtn.onClick.AddListener( () => {  NetworkManager.Singleton.StartHost(); CreatePlayerServerRpc(true); });
+        HostBtn.onClick.AddListener( () => {  NetworkManager.Singleton.StartHost(); CreatePlayerServerRpc(true); gameObject.SetActive(false); });
         ServerBtn.onClick.AddListener( () => { NetworkManager.Singleton.StartServer(); });
-        ClientBtn.onClick.AddListener( () => { NetworkManager.Singleton.StartClient(); CreatePlayerServerRpc(false); });
+        ClientBtn.onClick.AddListener( () => { NetworkManager.Singleton.StartClient();  CreatePlayerServerRpc(false); clickCount++; if (clickCount > 1) gameObject.SetActive(false); });
     }
 
     [ServerRpc(RequireOwnership = false)]
     private void CreatePlayerServerRpc(bool isHost, ServerRpcParams rpcParams = default)
     {
-        gameObject.SetActive(false);
         GameObject player = Instantiate(playerPrefab);
         if (isHost)
         {
             player.transform.position = new Vector3(0, 1.5f, 1);
-            gameInfo.SetYellowServerRpc(true);
         }
         else
         {
             player.transform.position = new Vector3(12, 1.5f, 1);
-            gameInfo.SetBlueServerRpc(true);
+            
         }
         player.GetComponent<NetworkObject>().SpawnWithOwnership(rpcParams.Receive.SenderClientId);
     }
 
     
+
+
+
+
 
 }
--- doors/Assets/UI/LevelEndUI.cs
+++ doors/Assets/UI/LevelEndUI.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelEndUI : MonoBehaviour
 {
+    [SerializeField] Button nextButton;
+    [SerializeField] Button quitButton;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        nextButton.onClick.AddListener(() => { NextLevel(); });
+        quitButton.onClick.AddListener(() => { QuitGame(); });
     }
 
-    // Update is called once per frame
-    void Update()
+    void NextLevel()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    void QuitGame()
+    {
+        Application.Quit();
     }
 }
